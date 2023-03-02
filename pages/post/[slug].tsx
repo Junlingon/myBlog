dfcf662a6acb56fbaa7d1c4d6d97b227411e340b
JunlinGong <913756523@qq.com>
2023-03-02 19:51:34 +0800

    feat: change post layout

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -11,15 +11,15 @@ const PostPage = () => {
 
     return (
         <div className="container mx-auto px-10 mb-8">
-            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-                <div className="col-span-1 lg:col-span-8">
+            <div className="grid lg:grid-cols-12 grid-cols-1 gap-12">
+                <div className="lg:col-start-2 lg:col-span-10 col-span-1">
                     <PostDetail post={{ e: "1" }} />
                 </div>
-                <div className="col-span-1 lg:col-span-4">
+                {/* <div className="col-span-1 lg:col-span-4">
                     <div className="relative lg:sticky top-8">
                         <PersonalWidget />
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
