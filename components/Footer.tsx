8de296ea4523401c8f5004b1dd14fc14b0a5ccb5
JunlinGong <913756523@qq.com>
2023-03-02 19:05:03 +0800

    feat: change footer to flex-wrap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className='container mx-auto px-10 '>
-            <div className="flex text-center justify-center text-gray-300 px-6 py-1">
+            <div className="flex flex-wrap justify-center text-gray-300 px-6 py-1">
                 @2023 code & design with ❥ by
                 <a className='px-2 underline' href="https://github.com/Junlingon" target="_blank">Alin</a>
             </div>
