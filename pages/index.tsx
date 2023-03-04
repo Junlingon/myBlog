fbc863f3a576a64d7c0927db7baaf35426d19f3b
JunlinGong <913756523@qq.com>
2023-03-04 14:02:15 +0800

    feat: Pagination feature implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,40 @@
 import type { NextPage, GetStaticProps } from 'next'
 import { useState } from 'react'
-import { PostCard, PersonalWidget, PostWidget, Categories, Pagination } from '../components'
+import { PostCard, PersonalWidget, PostWidget, Pagination } from '../components'
 import { getFileIds } from '../utils/posts-md'
-// const random = ['document', 'book', 'girl', 'art', 'nature']
-const posts = [
-  { title: "React Testing", excerpt: 'Learn React Testing', id: 1, random: 'nature' },
-  { title: "React with Tailwind", excerpt: 'Learn React', id: 2, random: 'art' },
-]
 
 interface Props {
   markdownFilesList: string[],
   random: string[],
 }
 
+const pageSize = 3
+
 const Home: NextPage<Props> = ({ markdownFilesList, random }) => {
   const [currentPage, setCurrentPage] = useState(1)
-  // const firstPageIndex = (currentPage - 1) * pageSize
-  // const lastPageIndex = firstPageIndex + pageSize
-  // const pageCount = Math.ceil(postsData.length / pageSize)
-  // const pagination = {
-  //   pageCount,
-  //   currentPage
-  // }
-
-  // const currentData = postsData.slice(firstPageIndex, lastPageIndex)
+  const firstPageIndex = (currentPage - 1) * pageSize
+  const lastPageIndex = firstPageIndex + pageSize
+  const pageCount = Math.ceil(markdownFilesList.length / pageSize)
+  const pagination = {
+    pageCount,
+    currentPage
+  }
+  const currentData = markdownFilesList.slice(firstPageIndex, lastPageIndex)
+  const recentData = markdownFilesList.slice(0, 5); // display 4 recent posts
 
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {markdownFilesList.map((post: any, index: number) => {
+          {currentData.map((post: any, index: number) => {
             return <PostCard post={post} key={index} random={random[index % 5]}></PostCard>
           })}
-          {2 > 1 && <Pagination pagination={{ currentPage: currentPage, pageCount: 2 }} onPageChange={(page: number) => setCurrentPage(page)} />}
+          {2 > 1 && <Pagination pagination={pagination} onPageChange={(page: number) => setCurrentPage(page)} />}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PersonalWidget />
-            <PostWidget posts={markdownFilesList} />
-            <Categories />
+            <PostWidget posts={recentData} />
           </div>
         </div>
       </div>
